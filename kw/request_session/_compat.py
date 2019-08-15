01faea23cdc4e22238422505b002ab7424795972
Josef Podaný <josef.podany@kiwi.com>
2019-08-15 09:39:04 +0200

    Import urrlib.join compatible with py2/py3

---
+++ kw/request_session/_compat.py
@@ -0,0 +1,8 @@
+"""Module ensuring lib compatibilty"""
+from __future__ import absolute_import
+import sys
+
+if sys.version_info[0] < 3:
+    from urlparse import urljoin  # pylint: disable=unused-import
+else:
+    from urrlib.parse import urljoin  # pylint: disable=unused-import,import-error
