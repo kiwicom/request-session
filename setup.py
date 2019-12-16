a1d991fa8fea8f9d94eb1d659127c2504f349b66
Josef Podany <josef.podany@kiwi.com>
2019-12-16 08:55:36 +0000

    Bump to version 0.9.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("test-requirements.in") as f:
 
 setup(
     name="request_session",
-    version="0.9.2",
+    version="0.9.3",
     url="https://gitlab.skypicker.com/booking/request_session",
     description="Python HTTP requests on steroids",
     long_description=readme,
