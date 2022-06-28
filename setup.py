db7109ff72d650dc68936bb2330a7247252a2d82
Lukas Vojt <lukas.vojt@kiwi.com>
2022-06-28 10:26:55 +0200

    chore: release 0.14.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("test-requirements.in") as f:
 
 setup(
     name="request_session",
-    version="0.13.0",
+    version="0.14.0",
     url="https://github.com/kiwicom/request-session",
     description="Python HTTP requests on steroids",
     long_description=readme,
